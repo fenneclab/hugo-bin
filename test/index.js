1e1111040853a6e2e92093ebe387ef52131c08b4
Jake Jarvis <jake@jarv.is>
2020-03-25 11:47:51 -0400

    Clean up tests

--- test/index.js
+++ test/index.js
@@ -5,21 +5,7 @@
 const assert = require('assert');
 const binCheck = require('bin-check');
 const hugoBin = require('..');
-const { execFile } = require('child_process');
 
-it('Hugo exists and runs?', () => {
-  return binCheck(hugoBin, ['version']).then(works => {
-    assert(works);
-
-    // Print additional build environment variables if check successful
-    if (works) {
-      execFile(hugoBin, ['env'], (error, stdout) => {
-        if (error) {
-          throw error;
-        }
-
-        console.log(stdout);
-      });
-    }
-  });
+it('Hugo exists and runs?', async () => {
+  assert(await binCheck(hugoBin, ['version']));
 });
