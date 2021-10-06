bf8f731e499f3a82c59c9407b143d707d9ff1d79
Jake Jarvis <jake@jarv.is>
2021-10-06 10:37:52 -0400

    increase timeout to an excessive 30 seconds for CI

--- test/index.js
+++ test/index.js
@@ -4,8 +4,10 @@ import { execFile } from "child_process";
 import assert from "assert";
 import hugo from "../index.js";
 
-it("Hugo exists and runs?", async () => {
-  assert(execFile(hugo, ["env"], (error, stdout) => {
+it("Hugo exists and runs?", async function () {
+  this.timeout(30000); // increase timeout to an excessive 30 seconds for CI
+
+  assert(execFile(hugo, ["env"], function (error, stdout) {
     if (error) {
       throw error;
     }
