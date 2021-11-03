7e1e17b6f18bd134ef31fe2c02da5cca2847dd6a
XhmikosR <xhmikosr@gmail.com>
2021-11-03 08:14:13 +0200

    test/index.js: switch to strict assert

--- test/index.js
+++ test/index.js
@@ -2,14 +2,14 @@
 
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert').strict;
 const binCheck = require('bin-check');
 const hugoBin = require('..');
 
 describe('hugo-bin', () => {
   it('should return path to binary and work', () => {
     return binCheck(hugoBin, ['version']).then(works => {
-      assert(works);
+      assert.ok(works);
     });
   });
 });
