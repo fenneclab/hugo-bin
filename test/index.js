4e3308dc3fd37f895385781cdaa75c5a06954956
XhmikosR <xhmikosr@gmail.com>
2021-12-07 09:41:59 +0200

    Switch to uvu for tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,14 @@
-/* eslint-env mocha */
-
 'use strict';
 
 const assert = require('assert').strict;
 const binCheck = require('bin-check');
 const hugoBin = require('..');
+const { test } = require('uvu');
 
-describe('hugo-bin', () => {
-  it('should return path to binary and work', () => {
-    return binCheck(hugoBin, ['version']).then(works => {
-      assert.ok(works);
-    });
+test('should return path to binary and work', () => {
+  return binCheck(hugoBin, ['version']).then(works => {
+    assert.ok(works);
   });
 });
+
+test.run();
