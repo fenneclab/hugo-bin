8a7caa62df65a7e4036e782ae59bd25b8e963156
Jens Weber <jens.weber@retarus.de>
2022-04-21 18:51:26 +0200

    refactor: switched to node strict

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
+import { strict as assert } from 'node:assert';
 import process from 'node:process';
 import binCheck from 'bin-check';
 import { test } from 'uvu';
 import hugoBin from '../index.js';
 import hugoLib from '../lib/index.js';
-import { strict as assert } from 'node:assert';
 
 test('should return path to binary and work', () => {
   return binCheck(hugoBin, ['version']).then(works => {
