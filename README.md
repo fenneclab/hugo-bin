cbb4db47923dc5d8d7045bc5d06bbcfce124d1bb
jweberde <weber.sdw@gmail.com>
2022-09-15 07:15:27 +0200

    feat: Allowing to overwrite the Download Repository aka Download Mirror (#120)
    introduces downloadRepo as new installation option allowing to define a mirror-repository

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ npm install hugo-bin --save-dev
 
 hugo-bin now supports the [Extended Hugo version](https://github.com/gohugoio/hugo/releases/tag/v0.43). See [Installation options](#installation-options) for more details.
 
+For usage within corporate networks or behind corporate proxies, the download repository can be overwritten, see [Installation options](#installation-options) for more details.
+
 ## Usage
 
 ### API
@@ -58,7 +60,7 @@ See the [Hugo Documentation](https://gohugo.io/) for more information.
 
 ## Installation options
 
-hugo-bin supports options to change the variation of Hugo binaries.
+hugo-bin supports options to change the variation of Hugo binaries and to overwrite the download repository.
 
 Each option can be configured in the `hugo-bin` section of your `package.json`:
 
@@ -67,7 +69,8 @@ Each option can be configured in the `hugo-bin` section of your `package.json`:
   "name": "your-package",
   "version": "0.0.1",
   "hugo-bin": {
-    "buildTags": "extended"
+    "buildTags": "extended",
+    "downloadRepo" : "https://some.example.com/artifactory/github-releases"
   }
 }
 ```
@@ -76,12 +79,14 @@ Also as local or global [.npmrc](https://docs.npmjs.com/files/npmrc) configurati
 
 ```ini
 hugo_bin_build_tags = "extended"
+hugo_bin_download_repo = "https://some.example.com/artifactory/github-releases"
 ```
 
 Also as an environment variable:
 
 ```sh
 export HUGO_BIN_BUILD_TAGS="extended"
+export HUGO_BIN_DOWNLOAD_REPO="https://some.example.com/artifactory/github-releases"
 ```
 
 **Note that you have to run `npm install hugo-bin` to re-install hugo-bin itself, if you change any of these options.**
@@ -95,6 +100,12 @@ Default: `""`
 Set it to `extended` to download the [extended version](https://github.com/gohugoio/hugo/releases/tag/v0.43) binary.
 
 If this is set to `extended` but it's not available for the user's platform, then the normal version will be downloaded instead.
+
+#### downloadRepo
+
+Default: `"https://github.com"`
+
+Set it to your corporate proxy url to download the hugo binary from a different download repository.
 
 ## Supported versions
 
--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 import { strict as assert } from 'node:assert';
+import process from 'node:process';
 import binCheck from 'bin-check';
-import { test } from 'uvu';
+import { test, suite } from 'uvu';
 import hugoBin from '../index.js';
+import hugoLib from '../lib/index.js';
 
 test('should return path to binary and work', () => {
   return binCheck(hugoBin, ['version']).then(works => {
@@ -10,3 +12,75 @@ test('should return path to binary and work', () => {
 });
 
 test.run();
+
+/**
+ * Verify Custom/Enterprise Repository overwrite.
+ */
+
+const hugoLibCustomRepoTestSuite = suite('hugo-bin overwrite download repository');
+
+hugoLibCustomRepoTestSuite.before.each(() => {
+  // Ensure that the environment is cleaned before next test run.
+  delete process.env.npm_config_hugo_bin_build_tags;
+  delete process.env.npm_config_hugo_bin_download_repo;
+});
+
+hugoLibCustomRepoTestSuite('verify test env', () => {
+  assert.equal(process.env.npm_config_hugo_bin_build_tags, undefined);
+  assert.equal(process.env.npm_config_hugo_bin_download_repo, undefined);
+})
+
+// Default Repository - Test Cases
+
+hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: undefined', () => {
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://github.com/'));
+  });
+});
+
+hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: empty', () => {
+  process.env.npm_config_hugo_bin_build_tags = '';
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://github.com/'));
+  });
+});
+
+hugoLibCustomRepoTestSuite('should return default repository url - Repository: default - Extended: extended', () => {
+  process.env.npm_config_hugo_bin_build_tags = 'extended';
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://github.com/'));
+  });
+});
+
+// Custom/Enterprise Repository Test Cases
+
+hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: undefined', () => {
+  process.env.npm_config_hugo_bin_download_repo = 'https://some1.example.com';
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://some1.example.com/'));
+  });
+});
+
+hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: empty', () => {
+  process.env.npm_config_hugo_bin_build_tags = '';
+  process.env.npm_config_hugo_bin_download_repo = 'https://some2.example.com';
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://some2.example.com/'));
+  });
+});
+
+hugoLibCustomRepoTestSuite('should return custom repository url - Repository: custom - Extended: extended', () => {
+  process.env.npm_config_hugo_bin_build_tags = 'extended';
+  process.env.npm_config_hugo_bin_download_repo = 'https://some3.example.com';
+  const repoSources = hugoLib(process.cwd())._src.map((v) => v.url);
+  repoSources.forEach((sourceUrl) => {
+    assert.ok(sourceUrl.startsWith('https://some3.example.com/'));
+  });
+});
+
+hugoLibCustomRepoTestSuite.run();
