fef870b1aa0ea1b69caf119650e334d876ff390d
XhmikosR <xhmikosr@gmail.com>
2026-04-16 14:53:22 +0300

    Move xo config to xo.config.js

---
+++ xo.config.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    space: 2,
+    rules: {
+      '@stylistic/comma-dangle': [
+        'error',
+        'never'
+      ],
+      '@stylistic/curly-newline': [
+        'error',
+        {
+          consistent: true
+        }
+      ],
+      '@stylistic/object-curly-spacing': [
+        'error',
+        'always'
+      ],
+      '@stylistic/operator-linebreak': [
+        'error',
+        'after'
+      ],
+      '@stylistic/space-before-function-paren': [
+        'error',
+        'never'
+      ],
+      'arrow-body-style': 'off',
+      camelcase: [
+        'error',
+        {
+          properties: 'never'
+        }
+      ],
+      'capitalized-comments': 'off',
+      curly: [
+        'error',
+        'multi-line'
+      ],
+      'prefer-template': 'error',
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/prevent-abbreviations': 'off'
+    }
+  }
+];
